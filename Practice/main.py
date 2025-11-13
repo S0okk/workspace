f962a0005b157641305653f22337aa4885477820
Nikita <sso0kk207@gmail.com>
2025-11-13 11:51:45 +0300

    updated

--- Practice/main.py
+++ Practice/main.py
@@ -1,29 +1,36 @@
-from fastapi import FastAPI, HTTPException, Response, Depends
-from authx import AuthX, AuthXConfig
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse, FileResponse
 
 app = FastAPI()
 
-config = AuthXConfig()
-config.JWT_SECRET_KEY = "supersecretkey"
-config.JWT_ACCESS_COOKIE_NAME = "my_access_token"
-config.JWT_TOKEN_LOCATION = ["cookies"]
+@app.post("/upload")
+async def upload_file(uploaded_file: UploadFile):
+    file = uploaded_file.file
+    filename = uploaded_file.filename
+    with open(f"1_{filename}", "wb") as f:
+        f.write(file.read())
 
-security = AuthX(config=config)
+@app.post("/multiple-upload")
+async def multiple_upload_file(uploaded_files: list[UploadFile]):
+    for uploaded_file in uploaded_files:
+        file = uploaded_file.file
+        filename = uploaded_file.filename
+        with open(f"1_{filename}", "wb") as f:
+            f.write(file.read())
 
-class UserLoginSchema(BaseModel):
-    username: str
-    password: str
+def iterfile(filename: str):
+    with open(filename, "rb") as f:
+        while chunk := f.read(1024 * 1024):
+            yield chunk
 
 
-@app.post("/login")
-async def login(creds: UserLoginSchema, response: Response):
-    if creds.username == "admin" and creds.password == "password":
-        token = security.create_access_token(uid="123")
-        response.set_cookie(key=config.JWT_ACCESS_COOKIE_NAME, value=token)
-        return {"access_token": token}
-    return HTTPException(status_code=401, detail="Invalid credentials")
 
-@app.get("/protected", dependencies=[Depends(security.access_token_required)])
-async def protected():
-    return {"data": "This is protected data"}
+# Show file on local storage
+@app.get("/files/{file_name}")
+async def get_file(file_name: str):
+    return FileResponse(file_name)
+
+# Show file as streaming response
+@app.get("/stream/{file_name}")
+async def stream_file(filename: str):
+    return StreamingResponse(iterfile(filename), media_type="text/plain")
