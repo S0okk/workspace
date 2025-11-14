a39bf1107ccf1fdeb7b398c4281a0ef470612f10
Nikita Sokolov <nikitasokolov@MacBook-Pro-de-Nikita.local>
2025-11-14 23:25:03 +0300

    some changes

--- Practice/main.py
+++ Practice/main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse, FileResponse
+import os
 
 app = FastAPI()
 
-@app.post("/upload")
+@app.post("/upload", tags=["Files"])
 async def upload_file(uploaded_file: UploadFile):
     file = uploaded_file.file
     filename = uploaded_file.filename
     with open(f"1_{filename}", "wb") as f:
         f.write(file.read())
 
-@app.post("/multiple-upload")
+@app.post("/multiple-upload", tags=["Files"])
 async def multiple_upload_file(uploaded_files: list[UploadFile]):
     for uploaded_file in uploaded_files:
         file = uploaded_file.file
@@ -26,11 +27,24 @@ def iterfile(filename: str):
 
 
 # Show file on local storage
-@app.get("/files/{file_name}")
-async def get_file(file_name: str):
-    return FileResponse(file_name)
+@app.get("/files/{file_name}", tags=["Files"])
+async def get_file(filename: str):
+    return FileResponse(filename)
 
 # Show file as streaming response
-@app.get("/stream/{file_name}")
+@app.get("/files/stream/{file_name}", tags=["Files"])
 async def stream_file(filename: str):
     return StreamingResponse(iterfile(filename), media_type="text/plain")
+
+# Create file
+@app.post("/files", tags=["Files"])
+async def create_file(filename: str, text: str):
+    with open(f"{filename}", "w") as f:
+        f.write(text)
+    return {"status": "success"}
+
+# Show all files in the sys
+@app.get("/files", tags=["Files"])
+async def get_files():
+    files_in_sys = os.listdir()
+    return {"Files in system": files_in_sys}
